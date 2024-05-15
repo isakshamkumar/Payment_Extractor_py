98b7f686db14d7e9d226d01da0b4919dd816d7bc
isakshamkumar <124489667+isakshamkumar@users.noreply.github.com>
2024-05-15 17:36:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,24 +6,19 @@ app = Flask(__name__)
 def receive_message_and_extract_amount():
     data = request.json
     messageArr = data.get('data')
-    print(messageArr)
     
     newArr = []
-    for  i in messageArr:
-        print(i)
+    for i in messageArr:
         pattern = r'\bRs:\s*(\d+\.\d{2})'
         import re
         match = re.search(pattern, i['message'])
         if match:
             amount = float(match.group(1))
             newArr.append(amount)
-            # return jsonify({'status': 'success', 'message': f'Extracted amount: {amount}'}), 200
         else:
-            # return jsonify({'status': 'error', 'message': 'No amount found in the message.'}), 400
             print(i)
     return jsonify({'status': 'success', 'amounts': newArr}), 200
-        # Example regex to extract amounts (adjust as needed)
-    
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Run the app on all available network interfaces and use the provided port
+    app.run(host='0.0.0.0', port=8080)
